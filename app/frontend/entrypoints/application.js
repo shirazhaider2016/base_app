95a375b880a90cfe410402bbfd7765041892a4c9
Shiraz <muhammad.shiraz@arkhitech.com>
2023-08-24 12:59:39 +0500

    Initial commit

---
+++ app/frontend/entrypoints/application.js
@@ -0,0 +1,13 @@
+import { Turbo } from "@hotwired/turbo-rails";
+
+window.Turbo = Turbo;
+
+import "./main.scss";
+
+import React, {createElement} from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '@/components/views/home';
+
+const domContainer = document.querySelector('#home');
+const home = createRoot(domContainer);
+home.render(createElement(App));
